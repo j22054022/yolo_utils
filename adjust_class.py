b6d126b552c39530523fbdc9705be54cbcc9d660
j22054022 <j22054022@gmail.com>
2022-08-04 14:51:49 +0800

    new feature adjust_class.py

--- adjust_class.py
+++ adjust_class.py
@@ -2,6 +2,7 @@ import os
 
 # abs path
 DIR_PATH = ''
+seek_class_index = ''
 fixed_class_index = ''
 file_list = os.listdir(DIR_PATH + '/')
 
@@ -9,12 +10,16 @@ for i in file_list:
     if i.find('.txt') > -1: 
         print(i)
         f = open(i, 'r+')
-        data = f.readline()
-        f.truncate(0)
+        # f.truncate(0)
         f.seek(0)
-        data = data.split(' ')
-        data[0] = fixed_class_index
-        data = ' '.join(data)
-        f.write(data)
-        # print(data, type(data))
+        lines = f.read().splitlines()
+        for data in lines:
+            original = data.split(' ')
+            data = data.split(' ')
+            if data[0] == seek_class_index:
+                data[0] = fixed_class_index
+                data = ' '.join(data)
+                f.write(f"{data}\n")
+                print(f"{' '.join(original)} -> {data}")
+            # print(data, type(data))
         f.close()
